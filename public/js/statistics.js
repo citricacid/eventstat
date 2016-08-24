62679e4796c4b4b8f093103ebcc50d75a1739bfe
hugo <hugo.osterberg@gmail.com>
2016-08-24 16:01:11 +0200

    Highchart popup

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -22,6 +22,7 @@ var toggleOption = function($this, isDisabled) {
     }
 };
 
+
 var setVisibleOptions = function($selector, data) {
     if (data === undefined) {
         $selector.find('option').show();
@@ -36,6 +37,7 @@ var setVisibleOptions = function($selector, data) {
         $selector.find(':visible:nth-child(2)').prop("selected", true);
     }
 };
+
 
 var resetCategories = function() {
     $('#category_selector').find('option:nth-child(2)').prop('selected', true);
@@ -67,27 +69,17 @@ $(function() {
     });
 
 
+    // handle radio buttons
     $('input[name=subtype_id]').change(function() {
       var categories = $(this).data('categories')
       || $('#event_maintype_selector').find(':selected').data('categories');
 
       setVisibleOptions($('#category_selector'), categories);
-      //$('#category_selector').change();
 
       var subcategories = $(this).data('subcategories')
         || $('#event_maintype_selector').find(':selected').data('subcategories');
 
       setVisibleOptions($('#subcategory_selector'), subcategories);
-
-    });
-
-
-    $('#category_selector').change(function() {
-      var subcategories = $(this).find(':selected').data('subcategories')
-        || $('input[name=subtype_id]:checked').data('subcategories')
-        || $('#event_maintype_selector').find(':selected').data('subcategories');
-
-      //setVisibleOptions($('#subcategory_selector'), subcategories);
     });
 
 
@@ -207,13 +199,68 @@ $(function() {
     var currentQuarter = moment(dateString, "DD-MM-YYYY").quarter();
     $("#select_quarter").val(currentQuarter).change();
 
-
-    // reset all menu options
-    //resetCategories();
-    //$('#branch_selector').find('option:first-child').prop('selected', true);
-
-    //$('.subtype_radio').hide();
-    //$('#event_maintype_selector').find('option:first-child').prop('selected', true);
     $('#event_maintype_selector').change();
+
+    // higcharts
+
+    $('#container').highcharts({
+            title: {
+                text: 'Monthly Average Temperature',
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'Source: WorldClimate.com',
+                x: -20
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: 'Temperature (°C)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: '°C'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Tokyo',
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }, {
+                name: 'New York',
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            }, {
+                name: 'Berlin',
+                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            }, {
+                name: 'London',
+                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            }]
+        });
+        $('[data-toggle=popover]').popover({
+        content: $('#container').html(),
+        html: true
+    }).click(function() {
+        $(this).popover('show');
+    });
+
+
+
+    // end hc
+
+
+
 
 });
