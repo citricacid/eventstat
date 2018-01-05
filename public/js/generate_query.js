549957deb7a9e308fc39745d7092520d0f40e536
hugo <hugo.osterberg@kul.oslo.kommune.no>
2018-01-05 16:15:53 +0100

    Prelim work on query generator

--- public/js/generate_query.js
+++ public/js/generate_query.js
@@ -2,10 +2,11 @@
 
 
 $(function() {
-  $('.item').dblclick(function() {
+  $('li').dblclick(function() {
+    const $parentRow = $(this).closest('div.row')
     const type = $(this).parent().data('type');
-    const sourceList =  $('ul.' + type + '.source');
-    const targetList =  $('ul.' + type + '.target');
+    const sourceList = $parentRow.find('ul.source');
+    const targetList = $parentRow.find('ul.target');
 
     const isFromSourceList = $(this).parent().hasClass('source');
 
@@ -18,6 +19,27 @@ $(function() {
 
     const destination = isFromSourceList ? targetList : sourceList;
     destination.append($(this));
+
+    updateFormElements()
   })
+
+
+  const updateFormElements = function() {
+    $('ul.target').each(function(i, targetList) {
+      let values = $(this).find('li').map(function() {return $(this).attr('value')}).get()//.join('0')
+      let name = $(this).data('type')
+
+      $('[name=' + name + ']').val(values)
+    })
+  }
+
+  $('.list_show').click(function() {
+    $(this).closest('div.row').find('.sourcepanel').show()
+  })
+
+  $('.list_hide').click(function() {
+    $(this).closest('div.row').find('.sourcepanel').hide()
+  })
+
 
 });
