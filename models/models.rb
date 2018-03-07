da504510c8e38b19fce5e48c510fb3ca73b589df
hugo <hugo.osterberg@kul.oslo.kommune.no>
2018-03-07 15:06:36 +0100

    Minor bug fix

--- models/models.rb
+++ models/models.rb
@@ -332,7 +332,7 @@ class Category < ActiveRecord::Base
   end
 
   def maintype_associated?(maintype_id)
-    event_maintype.id == maintype_id.to_i
+    maintype_id && event_maintype.id == maintype_id.to_i
   end
 
   def subtype_associated?(subtype_id)
